twomice/com.joineryhq.profcond

commit 41f95afb43f46a9e6d3c99285132fe103dbfc77a

Fix #29: some rules being overridden by CiviCRM in-line javascript

js/profcond.js
@@ -1,4 +1,4 @@
-(function ($, ts) {
+CRM.$(function ($, ts) {
 
   // Define special handler for select2 elements.
   var ps2 = new profcondSelect2($);

commit 68c254a109aa0ec3b124effe6841a18dcd8444b2

Bug-fix: REGRESSION: JS error introduced in 41f95afb43f46a9e6d3c99285132fe103dbfc77a ("Fix #29").

js/profcond.js
@@ -439,4 +439,4 @@ CRM.$(function ($, ts) {
     $('form#' + CRM.vars.profcond.formId).submit(profcondStoreHidden);
   }
 
-})(CRM.$, CRM.ts('com.joineryhq.profcond'));
+});